f5dec5ed0f087aad79f90230bd63da998b51ab99
CosmoLustosa <cosmolustosa2@gmail.com>
2021-01-29 20:55:28 -0300

    project created

---
+++ src/test/java/br/cosolustosa/curso/CursoNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.cosolustosa.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
