1dea71dba190e347dfbe61f11a028817f289e905
CosmoLustosa <cosmolustosa2@gmail.com>
2021-01-29 21:38:37 -0300

    User entity and resource

---
+++ src/main/java/br/cosolustosa/curso/resources/UserResource.java
@@ -0,0 +1,21 @@
+package br.cosolustosa.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.cosolustosa.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User usuario = new User(1L, "Cosmo Lustosa", "cosmolustosa2@gmail.com", "99 9967-0239", "1234");
+		return ResponseEntity.ok().body(usuario);
+	}
+
+}
